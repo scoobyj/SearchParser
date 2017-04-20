33a4e132c433d43a643a7208c6fb94164f67ce0d
scoobyj <jeffkjohn99@gmail.com>
2017-04-19 21:56:23 -0400

    Add files via upload

--- SearchParser.py
+++ SearchParser.py
@@ -1,54 +1,73 @@
 from sys import version_info
-import glob, os , pprint ,os.path, re, datetime, webbrowser, subprocess,sys,argparse
+import glob, os , pprint ,os.path, re, webbrowser, subprocess,sys,argparse,operator
 from itertools import  groupby , dropwhile , chain
 from dateutil.parser import parse
 from collections import  defaultdict
+from datetime import datetime
 
 
-pat_entry = "(\\[.*?\\])\\s(\\S+)\\s(\\S+)\\s\\S\\s+(PerfLog)\\s(<entry operation)(.*)"
-pat_exit = "(\\[.*?\\])\\s(\\S+)\\s(\\S+)\\s\\S\\s+(\\S+)\\s(<exit operation)(.*)"
-tentry = re.compile(pat_entry)
-texit = re.compile(pat_exit)
-pat_t = ("(\\[.*?\\])\\s(\\S+).*?")
-thtrc = re.compile(pat_t)
 
 
-def getfiles(response):
-    files = filter(os.path.isfile, glob.glob(os.path.join(response, "trace*.log")))
-    files.sort(key=lambda x: os.path.getmtime(x))
-    return files
+linpat = "^\\[(\\d+?\\/\\d+?\\/\\d+?\\s\\d+?:\\d+?:\\d+?:\\d+?)\\s\\w+\\]\\s([0-9A-Fa-f]+)\\s=?(\\w+)\\s(\\S)(.*)"
+lincomp = re.compile(linpat)
+
+class LData(object):
+    """ Produces object that makes up a line"""
+    def __init__(self,datime,tid,comp,op,payload):
+        self.datime = datime
+        self.tid = tid
+        self.comp = comp
+        self.op = op
+        self.payload = payload
+
+def ParseTime(tmpdatime):
+        datm = datetime.strptime(tmpdatime, '%m/%d/%y %H:%M:%S:%f')
+        return datm
             
-      
-
-def groupLinesByMatch(file):
-    result = defaultdict(list)
-
-    for line in open(file).readlines():
-        matches = thtrc.match(line)
-        if matches:    
-            result[matches.group(2)].append( line )
-    return result.values()
-
-
-                   
-                
-
-def main():
-    py3 = version_info[0] > 2  # creates boolean value for test that Python major version > 2
-    if py3:
-        response = input("Please enter directory where search traces resides;   ")
-    else:
-        response = raw_input("Please enter directory where search traces resides: ")
+def parsetoFields(filename):
+        print("opening " + filename)
+        with open(filename,'r+') as f:
+            tlines = []
+            for line in f:
+                t = lincomp.search(line)
+                if t:
+                    #datime = ParseTime(t.group(1))
+                    datm = datetime.strptime(t.group(1), '%m/%d/%y %H:%M:%S:%f')
+                    print (datm)
+                    tid = t.group(2)
+                    comp = t.group(3)
+                    op = t.group(4)
+                    payload = t.group(5)
+                    data = LData(datm,tid,comp,op,payload)
+                    tlines.append(datm)
+                    tlines.append(tid)
+                    tlines.append(comp)
+                    tlines.append(op)
+                    tlines.append(payload)
+                    
+        return tlines
     
-    files = getfiles(response)
-    for file in files:
-        #threads = pullThreads(file,response)
-        for lines in groupLinesByMatch(file):
-            for line in lines:
-                tent = tentry.search(line)
-                if tent:
-                    print ''.join(line)
-
+    
+def main():
+    try:
+        response=sys.argv[1]
+        print(response)
+    except:
+        py3 = version_info[0] > 2  
+        if py3:
+            response = input("Please enter directory where search traces resides;   ")
+        else:
+            response = raw_input("Please enter directory where search traces resides: ")
+    tlines = []
+    combolines = []
+    for filename in glob.glob(os.path.join(response,'trace*.log')):
+        tlines = parsetoFields(filename)
+        if tlines:
+            combolines.extend(tlines)
+            s_comblines = sorted(combolines, key=lambda LData: LData.datetime, reverse=True)
+            print "\n".join(s_comblines)
+  
+        
 
 
 if __name__ == "__main__":
